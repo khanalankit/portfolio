ca70a43b04705e4a4c2a5c8c0465247a6b28f11b
Darshan Gautam <dsrsangautam@gmail.com>
2022-04-15 15:02:33 +0545

    done form validation

--- src/pages/Contact_me.js
+++ src/pages/Contact_me.js
@@ -1,16 +1,83 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import "./Contact_me.css"
 const Contact_me = () => {
+  const [name,setName]= useState("");
+  const [email,setEmail]= useState("");
+  const [message,setMessage] = useState("");
+
+  const [nameErr,setNameErr] = useState({});
+  const [emailErr,setEmailErr] = useState({});
+  const [messageErr,setMessageErr] = useState({});
+
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    const isValid = formValidation();
+    if(isValid){
+      //send this data to database or api
+      setName("")
+      setEmail("")
+      setMessage("")
+    }
+  }
+
+  const formValidation = ()=>{
+    const nameErr = {};
+    const emailErr = {};
+    const messageErr = {};
+    let isValid = true;
+    if(name.trim().length<1){
+      nameErr.nameEmpty = "Name is required";
+      isValid = false;
+
+    }
+    if(!email.includes("@gmail.com")){
+      emailErr.email123 = "Invalid mail!!";
+      isValid= false;
+    }
+    if (message.trim().length<10){
+      messageErr.msgtxt = "atleast 10 letters is required!!";
+      isValid = false;
+    }
+    setNameErr(nameErr);
+    setEmailErr(emailErr);
+    setMessageErr(messageErr);
+    return isValid;
+
+  }
   return (
     <div className='contact_container'>
-    <form action="#"id='contact-form'>
+    <form action="#"id='contact-form' onSubmit={onSubmit}>
         <label for="name" id="name-label">Name</label><br/>
-        <input type="text" id="name" name="name" placeholder="Enter your name" required/><br/><br/>
+        <input type="text" 
+            id="name" name="name" 
+            placeholder="Enter your name" 
+            value={name}
+            onChange = {(e)=>{setName(e.target.value)}}/><br/><br/>
+
+        {Object.keys(nameErr).map((key)=>{
+          return <div style={{color:"red"}}>{nameErr[key]}</div>
+        })}
+          
         <label for="email" id="email-label">Email</label><br/>
-        <input type="email" id="email" name="email" placeholder="Enter your email" required/><br/><br/>
-        <span className='message_text'>Message</span><br/>   
-        <textarea name="data" id="textarea" cols="90" rows="10" placeholder="Enter your message here...."></textarea>
+        <input type="email"
+             id="email" name="email" 
+             placeholder="Enter your email" 
+             value={email}
+             onChange={(e)=>{setEmail(e.target.value)}} /><br/><br/>
+
+        {Object.keys(emailErr).map((key)=>{
+          return <div style={{color:"red"}}>{emailErr[key]}</div>
+        })}
+        <span className='message_text'>Message</span><br/> 
+
+        <textarea name="data" id="textarea" cols="90" rows="10" placeholder="Enter your message here...."
+            value={message}
+            onChange = {(e)=>{setMessage(e.target.value)}}
+            />
         <br/><br/>
+        {Object.keys(messageErr).map((key)=>{
+          return <div style={{color:"red"}}>{messageErr[key]}</div>
+        })}
         <input type="submit" value="Submit" id="submit"/> 
     </form>
 </div>
