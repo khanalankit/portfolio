ffc3059b4f0087aeed4692f4d5af73fc804edd60
Darshan Gautam <dsrsangautam@gmail.com>
2022-04-15 16:22:31 +0545

    added images to the card components

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Card.css'
+import antique from '../../assets/antique.jpg'
+import hardware from '../../assets/hardware.jpg'
+import screen from '../../assets/screen.jpg'
+
 function Card(props){
+    var pictures={
+        0:antique,
+        1:hardware,
+        2:screen
+    }
     return(
             <div className="cards">
+                <img src={pictures[Math.floor(Math.random()*3)]} alt="some random image" className='pic'/>
                 <div className="card_header"><h4>{props.card_header}</h4></div>
-                <div className="card_body">{props.card_body}</div>
+                <div className="card_body">{props.card_body?props.card_body:"This is a project click below to see more."}</div>
                 <div className="card_footer">
                     <a href={props.link} target="_blank"><button type="button">View source</button></a>
                 </div>
