cadfbcb05d9020b42913e2f32238684935eadedf
Darshan Gautam <dsrsangautam@gmail.com>
2022-04-27 20:47:01 +0545

    added button on front page and changed contact form

--- src/pages/Contact_me.js
+++ src/pages/Contact_me.js
@@ -1,54 +1,50 @@
-import React,{useEffect,useState} from 'react'
-import "./Contact_me.css"
+import React, { useEffect, useState } from "react";
+import "./Contact_me.css";
 import screen from "../assets/screen.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Contact_me = () => {
-  const [name,setName]= useState("");
-  const [email,setEmail]= useState("");
-  const [message,setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  const [nameErr,setNameErr] = useState({});
-  const [emailErr,setEmailErr] = useState({});
-  const [messageErr,setMessageErr] = useState({});
+  const [nameErr, setNameErr] = useState({});
+  const [emailErr, setEmailErr] = useState({});
+  const [messageErr, setMessageErr] = useState({});
 
-  const onSubmit = (e)=>{
+  const onSubmit = (e) => {
     e.preventDefault();
     const isValid = formValidation();
-    if(isValid){
+    if (isValid) {
       //send this data to database or api
-      setName("")
-      setEmail("")
-      setMessage("")
+      setName("");
+      setEmail("");
+      setMessage("");
     }
-  }
-var nameRegex = /^[ a-zA-Z]+$/;
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+  };
+  var nameRegex = /^[ a-zA-Z]+$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
-  const formValidation = ()=>{
+  const formValidation = () => {
     const nameErr = {};
     const emailErr = {};
     const messageErr = {};
     let isValid = true;
-    if(name.trim().length<1){
+    if (name.trim().length < 1) {
       nameErr.nameEmpty = "Name is required";
       isValid = false;
-
-    }
-    else if(!name.match(nameRegex)){
+    } else if (!name.match(nameRegex)) {
       nameErr.numName = "Invalid name";
       isValid = false;
-
     }
-    if(email.trim().length<1){
+    if (email.trim().length < 1) {
       emailErr.emailemp = "Email is required";
       isValid = false;
-
-    }
-    else if(!email.match(emailRegex)){
+    } else if (!email.match(emailRegex)) {
       emailErr.emailName = "Invalid email";
       isValid = false;
-
     }
-    if (message.trim().length<10){
+    if (message.trim().length < 10) {
       messageErr.msgtxt = "atleast 10 letters is required!!";
       isValid = false;
     }
@@ -56,46 +52,88 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
     setEmailErr(emailErr);
     setMessageErr(messageErr);
     return isValid;
-
-  }
+  };
   return (
-    <div className='contact_container'>
-    <form action="#"id='contact-form' onSubmit={onSubmit}>
-        <label for="name" id="name-label">Name</label><br/>
-        <input type="text" 
-            id="name" name="name" 
-            placeholder="Enter your name" 
-            value={name}
-            onChange = {(e)=>{setName(e.target.value)}}/><br/><br/>
+    <div className="contact_container">
+      <form action="#" id="contact-form" onSubmit={onSubmit}>
+        <h1 className="contactMe">Contact Me</h1>
+        <label for="name" id="name-label">
+          Name
+        </label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <br />
+        <br />
 
-        {Object.keys(nameErr).map((key)=>{
-          return <div style={{color:"red"}}>{nameErr[key]}</div>
+        {Object.keys(nameErr).map((key) => {
+          return (
+            <div style={{ color: " #FF5733 ", marginTop: "-20px" }}>
+              {nameErr[key]}
+            </div>
+          );
         })}
-          
-        <label for="email" id="email-label">Email</label><br/>
-        <input type="email"
-             id="email" name="email" 
-             placeholder="Enter your email" 
-             value={email}
-             onChange={(e)=>{setEmail(e.target.value)}} /><br/><br/>
 
-        {Object.keys(emailErr).map((key)=>{
-          return <div style={{color:"red"}}>{emailErr[key]}</div>
+        <label for="email" id="email-label">
+          Email
+        </label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+
+        {Object.keys(emailErr).map((key) => {
+          return (
+            <div style={{ color: " #FF5733 ", marginTop: "-20px" }}>
+              {emailErr[key]}
+            </div>
+          );
         })}
-        <span className='message_text'>Message</span><br/> 
-
-        <textarea name="data" id="textarea" cols="90" rows="10" placeholder="Enter your message here...."
-            value={message}
-            onChange = {(e)=>{setMessage(e.target.value)}}
-            />
-        <br/><br/>
-        {Object.keys(messageErr).map((key)=>{
-          return <div style={{color:"red"}}>{messageErr[key]}</div>
+        <br />
+        <span className="message_text">Message</span>
+        <br />
+        <br />
+        <textarea
+          name="data"
+          id="textarea"
+          cols="90"
+          rows="10"
+          placeholder="Enter your message here"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        {Object.keys(messageErr).map((key) => {
+          return (
+            <div style={{ color: " #FF5733 ", marginTop: "-20px" }}>
+              {messageErr[key]}
+            </div>
+          );
         })}
-        <input type="submit" value="Submit" id="submit"/> 
-    </form>
-</div>
-  )
-}
+        <input type="submit" value="Submit" id="submit" />
+      </form>
+    </div>
+  );
+};
 
-export default Contact_me
+export default Contact_me;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,18 +1,30 @@
-import React from 'react'
-import "./Homepage.css"
-import profile from "../assets/profile_pic1.png"
+import React from "react";
+import "./Homepage.css";
+import profile from "../assets/profile-removebg.png";
+import resume from "../assets/Resume.pdf";
 
 const Homepage = () => {
   return (
-    <div className='hero_container'>
-      <div className='des_container'>
-      <h1>Darshan Gautam</h1>
-      <p>Bachelors in Electronics and Communication Engineering.<br/>Software Engineer intern at Optimum Futurist.</p>
-      <button type="button">Contact Me</button>
+    <div className="hero_container">
+      <div className="des_container">
+        <h1>Darshan Gautam</h1>
+        <p>
+          Bachelors in Electronics and Communication Engineering.
+          <br />
+          Software Engineer intern at Optimum Futurist.
+        </p>
+        <div className="home_Btn">
+          <a href={resume} download="resume">
+            <button type="button">Download CV</button>
+          </a>
+          <a href="/Contact_Me">
+            <button type="button">Contact Me</button>
+          </a>
+        </div>
       </div>
-      <img className='profile' src={profile} alt="profile picture"/>
+      <img className="profile" src={profile} alt="profile picture" />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
